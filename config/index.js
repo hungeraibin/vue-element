cd80f6c3d163cfb55d0a2f96e0aeff6d61391a12
王爱彬 <hungeraibin@126.com>
2017-12-20 18:13:26 +0800

    node开启服务器

--- config/index.js
+++ config/index.js
@@ -57,6 +57,10 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
+    // Various Dev Server settings
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 9000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+
     /**
      * Source Maps
      */
